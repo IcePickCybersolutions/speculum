fc74c8f535775d6b803021f95eee23ac288ef197
IcePickCyberSolutions <80115033+IcePickCybersolutions@users.noreply.github.com>
2021-04-08 10:10:06 -0600

    Add files via upload

---
+++ speculum.py
@@ -0,0 +1,39 @@
+import os
+
+print('''
+                                 _                 
+                                | |                
+  ___  ___ _ __   ___  ___ _   _| |_   _ _ __ ___  
+ / __|/ __| '_ \ / _ \/ __| | | | | | | | '_ ` _ \ 
+ \__ \ (__| |_) |  __/ (__| |_| | | |_| | | | | | |
+ |___/\___| .__/ \___|\___|\__,_|_|\__,_|_| |_| |_|
+          | |                                      
+          |_|                                      
+
+Making deepfakes as easy as 123,ABC, you and me!
+
+Ingredients:
+- python (the language, but snakes are pretty cool)
+- pip (thingy that helps download python stuff)
+- the code from my github (all in one folder)
+- a photo to animate (in code folder)
+- a video to animate the photo to do (again, same folder)
+- a big idea (at least one cup)
+
+Directions are available on my github page. 
+If you're seeing this and those steps are finished, just answer some of my questions...
+''')
+
+qimage = input("what is your source image named?: ")
+qvideo = input("what is your driving video named?: ")
+qgpu = input("do you have a bespoke gpu?: ")
+
+if qgpu == no:
+	os.system("deep_animate", qimage, qvideo, "conf.yaml nameofmodelweights")
+	
+elif gpu == yes:
+	os.system("deep_animate", qimage, qvideo, "conf.yaml nameofmodelweights --device cuda")
+
+else: print("Incorrect input, please answer the gpu question with either yes or no")
+
+print("Your video is now saved to the folder with the code, process complete")
